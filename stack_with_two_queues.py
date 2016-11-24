9aec1cac84a66e77a1c09823b80334658eebce51
guven <g.candogan@buildingradar.com>
2016-11-24 14:09:48 +0100

    stack with two queues

---
+++ stack_with_two_queues.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+__author__ = 'guven'
+
+
+class StackWithTwoQueues(object):
+
+    def __init__(self):
+        self.q_first = []
+        self.q_second = []
+
+    @staticmethod
+    def _transfer(self, q1, q2):
+        pass
+
+    def push(self, new_item):
+        pass
+
+    def pop(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    s = StackWithTwoQueues()
+    s.push(1)
+    print s.pop()
